29276376af2056a4dba92db0ec867291a18bde7c
Gep54 <beranva6@fel.cvut.cz>
2026-01-02 19:09:41 +0100

    asi jako6e huraaaa

--- ImgProcessing.py
+++
@@ -1,7 +0,0 @@
-from ctu_crs import CRS97
-import numpy as np
-import opencv as cv
-
-robot.initialize(home=False)
-
-
--- solution.py
+++ solution.py
@@ -16,20 +16,21 @@ def move_robot_to(position, robot):
     robot.wait_for_motion_stop()
 
 
-def solution(puzzle, robot):
+def solution(puzzle, robot, T_RC):
     robot.soft_home()
     cam = camera.Camera()
     img = robot.grab_image()
     midpoint, R = cam.get_mid_points(img)
+    midpoint[2] -= 35
     trajectory_R = puzzle.get_reverse_trajectory()
     trajectory_F = trajectory_R[::-1]
     trajectory_F[:, 2] *= -1
     trajectory_F_camera_coord = trajectory_F @ R + midpoint
-    trajectory_F_robot_coord = [cam.cameraToRobot(x) for x in trajectory_F_camera_coord]
-    for p in trajectory_F_robot_coord:
-        p[2] += 0.05
-    start = trajectory_F_robot_coord[0]
-    start[2] = start[2] + 0.05
+    trajectory_F_robot_coord = [cam.cameraToRobot(x, T_RC) for x in trajectory_F_camera_coord]
+    # for p in trajectory_F_robot_coord:
+    #     p[2] += 0.05
+    start = trajectory_F_robot_coord[0].copy()
+    start[2] += 0.05
     arm_vector = np.array([-1, 1, 0])
     normal_vector = np.array([0, 0, 1])
     StartPose = PoseComposer.make_se3_matrix(start, arm_vector, normal_vector)
